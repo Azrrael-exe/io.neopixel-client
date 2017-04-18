dfb2e9d98e2d03fc7fc916bebbf99f90b196e7b8
Victor Camargo <vcamargo.e@gmail.com>
2017-04-18 02:24:32 -0500

    Arduino code added

--- Arduino-Firmware/src/main.ino
+++ Arduino-Firmware/src/main.ino
@@ -1,7 +1,27 @@
-void setup(){
+#include <Adafruit_NeoPixel.h>
+#include <ArduinoJson.h>
 
+#define PIXEL_PIN 3
+#define PIXEL_COUNT 4
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup(){
+  pixels.begin();
+  Serial.begin(115200);
 }
 
 void loop(){
-  
+  if(Serial.available() > 0){
+    StaticJsonBuffer<200> jsonBuffer;
+    String inp = Serial.readStringUntil('\n');
+    JsonObject& root = jsonBuffer.parseObject(inp);
+    if(root.success()){
+      // command = root["command"].asString();
+      // red = root["red"];
+      // green = root["green"];
+      // blue = root["blue"];
+      // index = root["index"];
+    }
+  }
 }
