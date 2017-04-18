1915573805fecf0c84a954c57188426c45a46947
Victor Camargo <vcamargo.e@gmail.com>
2017-04-17 19:04:41 -0500

    initial commit

---
+++ client.js
@@ -0,0 +1,31 @@
+var io = require("socket.io-client")
+var socket = io('https://io-neopixel.herokuapp.com/');
+// var SerialPort = require('serialport');
+// var port = new SerialPort('COM3',{
+//   baudrate : 115200
+// });
+//
+// port.on('open', function(){
+//   console.log("SerialPort open!")
+// });
+
+socket.on('connect', function(){
+  console.log(socket.id);
+  socket.emit('register', {
+    socketName : 'SocketTest',
+    socketId : socket.id
+  })
+  socket.emit('user-command', {
+    socketName : 'SocketTest',
+    payload : {
+      red : 100,
+      green : 0,
+      blue : 100
+    }
+  })
+});
+
+socket.on('device-command', function(msg){
+  // port.write('main screen turn on', function(err){});
+  console.log(msg)
+})
